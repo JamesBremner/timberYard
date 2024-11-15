a126a15b3bb4d099b04f1c3c321dfc891f5c1023
JamesBremner <james@ravenspoint.com>
2024-11-15 09:38:59 -0500

    open GUI

---
+++ src/cGUI.h
@@ -0,0 +1,58 @@
+class cGUI
+{
+public:
+    /** CTOR
+     * @param[in] title will appear in application window title
+     * @param[in] vlocation set location and size of appplication window
+     * 
+     * Usage:
+     * 
+     * <pre>
+class appGUI : public cStarterGUI
+{
+public:
+    appGUI()
+        : cStarterGUI(
+              "The Appliccation",
+              {50, 50, 1000, 500})
+    {
+
+        // initialize the solution
+        ...
+
+        show();
+        run();
+    }
+    </pre>
+    */
+    cGUI(
+        const std::string &title,
+        const std::vector<int> &vlocation)
+        : fm(wex::maker::make())
+    {
+        fm.move(vlocation);
+        fm.text(title);
+
+        fm.events().draw(
+            [&](PAINTSTRUCT &ps)
+            {
+                wex::shapes S(ps);
+                draw(S);
+            });
+
+        fm.show();
+        fm.run();
+    }
+    /** Draw nothing
+     * 
+     * An application should over-ride this method
+     * to perform any drawing reuired
+     */
+    virtual void draw( wex::shapes& S )
+    {
+
+    }
+
+protected:
+    wex::gui &fm;
+};
---
+++ src/timberGUI.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <memory>
+#include <wex.h>
+#include "cGUI.h"
+#include "TimberAllocation.h"
+
+main()
+{
+    cGUI theGUI(
+              "Timber Yard",
+              {50, 50, 1000, 500});
+    return 0;
+}
